6b08ab7dede17bb68ffe97d143f2a2e841a77fdc
Mayass Ali <157918175+mibo01699@users.noreply.github.com>
2026-09-01 20:58:55 +0300

    Update tokenomics.test.js

--- tests__/tokenomics.test.js
+++ tests__/tokenomics.test.js
@@ -1,8 +1,20 @@
 const YER_TOKENOMICS = require('../YERTokenomicsCanonical.js');
 
 test('Tokenomics allocations sum to maximum supply', () => {
-  const sum = YER_TOKENOMICS.allocations.communityPublicUtility +
-               YER_TOKENOMICS.allocations.ecosystemLaunchLiquidity +
-               YER_TOKENOMICS.allocations.aecSovereignReserve;
-  expect(sum).toBe(YER_TOKENOMICS.maximumSupply);
+  const total = YER_TOKENOMICS.allocations.communityPublicUtility +
+                YER_TOKENOMICS.allocations.ecosystemLaunchLiquidity +
+                YER_TOKENOMICS.allocations.aecSovereignReserve;
+
+  expect(total).toBe(YER_TOKENOMICS.maximumSupply);
+});
+
+test('Percentages are correct', () => {
+  const communityRatio = Number(YER_TOKENOMICS.allocations.communityPublicUtility) / Number(YER_TOKENOMICS.maximumSupply);
+  expect(communityRatio).toBeCloseTo(0.1, 2);
+
+  const ecosystemRatio = Number(YER_TOKENOMICS.allocations.ecosystemLaunchLiquidity) / Number(YER_TOKENOMICS.maximumSupply);
+  expect(ecosystemRatio).toBeCloseTo(0.3, 2);
+
+  const reserveRatio = Number(YER_TOKENOMICS.allocations.aecSovereignReserve) / Number(YER_TOKENOMICS.maximumSupply);
+  expect(reserveRatio).toBeCloseTo(0.6, 2);
 });
